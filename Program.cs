b3e2082f77c32a007cb96803fe93cee6acee5106
phum <aekkaratthaphum@outlook.co.th>
2023-03-13 16:39:15 +0700

    fixed bug

--- Program.cs
+++ Program.cs
@@ -13,17 +13,22 @@ namespace midtermExam{
             float w2 =float.Parse(Console.ReadLine());
             float h2 =float.Parse(Console.ReadLine());
 
-            
+            /*
             float[] l1 = {x1+w1-h1,y1+w1-h1};
             float[] r1 = {x1+w1+h1,y1+w1+h1};
             float[] l2 = {x2+w1-h1,y2+w2-h2};
             float[] r2 = {x2+w2+h2,y2+w2+h2};
-/*
+            */
+            float[] l1 = {x1-w1,y1-h1};
+            float[] r1 = {x1+w1,y1+h1};
+            float[] l2 = {x2-w2,y2-h2};
+            float[] r2 = {x2+w2,y2+w2};
+
             Console.WriteLine(l1[0] + "," + l1[1]);
             Console.WriteLine(r1[0] + "," + r1[1]);
             Console.WriteLine(l2[0] + "," + l2[1]);
             Console.WriteLine(r2[0] + "," + r2[1]);
-            */
+            
             Console.WriteLine(CheckAnwser(overlappingArea(l1,r1,l2,r2)));
 
             
@@ -34,6 +39,7 @@ namespace midtermExam{
 
             float area1 = Math.Abs(l1[0] - r1[0]) * Math.Abs(l1[0] - r1[0]);
             float area2 = Math.Abs(l2[1] - r2[1]) * Math.Abs(l2[1] - r2[1]);
+
             float x_dist = (Math.Min(r1[0], r2[0]) - Math.Max(l1[0], l2[0]));
             float y_dist = (Math.Min(r1[1], r2[1]) - Math.Max(l1[1], l2[1]));
             
@@ -44,7 +50,7 @@ namespace midtermExam{
              if (x_dist > 0 && y_dist > 0) {
                      areaI = x_dist * y_dist;
             }
-           // Console.WriteLine(areaI);
+            //Console.WriteLine(areaI);
             return (areaI);
         }
 
